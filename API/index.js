d0d71e8f3c4d7a8f9446e06a08dd94ffb65643f1
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-07 09:34:56 +0200

    Changes to index.js

--- API/index.js
+++ API/index.js
@@ -8,9 +8,9 @@ const {errorHandling} = require('./middleware/ErrorHandling');
 
 app.use((req, res, next)=> {
     res.header('Access-Control-Allow-Origin', 'http:/localhost:8080');
-    res.header('Access-Control-Allow-Credentials', "true");
-    res.header('Access-Control-Allow-Methods', "*");
-    res.header('Access-Control-Allow-Headers', "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
 });
 app.use(route);
