d6afb3b1fa938dcd9d06887b737ee920e3009688
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-08 11:27:17 +0200

    Server Updated

--- API/index.js
+++ API/index.js
@@ -13,6 +13,7 @@ app.use((req, res, next)=> {
     res.header("Access-Control-Allow-Headers", "*");
     next();
 });
+
 app.use(route);
 app.use(cors(), cookieParser(), express.json, express.urlencoded({extended: false}));
 
