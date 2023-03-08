a6c349dea9ab0d803faeaf4e16b7c09b781c3175
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-08 16:15:25 +0200

    Views Created and Displayed

--- stygian-umbra-gaming/src/views/HomeView.vue
+++ stygian-umbra-gaming/src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="container-fluid sect">
+      <div class="wrapper text-center">
+          <div class="rounded-3 animated-charcter" data-aos="fade-down">
+              <h2 class="text-white kage display-6 fw-bolder">Portfolio Of</h2>
+          </div>
+          <figure>
+              
+              <figcaption class="animated-charcter fw-semibold">
+                  <p class="text-light"><strong>Aspiring Developer | Aspiring Web Designer | Aspiring Coder</strong></p>
+              </figcaption>
+          </figure>
+      </div>
+  </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+<style>
+.home{
+    margin-top: 9rem;
+}
+figcaption{
+    background-color: rgb(36, 54, 54);
+}
+.image-round{
+    width: 600px;
+    height: 400px;
+    border-radius: 1500px;
+}
+.image-rounded{
+    width: 1000px;
+    height: 400px;
+    border-radius: 1500px;
+}
+</style>
