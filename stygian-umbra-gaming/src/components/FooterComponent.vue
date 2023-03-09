6bf939107c38d736bf95b5dedf204ca6b8689b86
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-09 10:05:25 +0200

    Footer

--- stygian-umbra-gaming/src/components/FooterComponent.vue
+++ stygian-umbra-gaming/src/components/FooterComponent.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
     <footer class="bg-dark text-white">
-      <p class="fw-bolder">
+      <p class="copy">
         Copyright &copy; 2023
-        <a href="#"
-          ><img
-            src="https://img.icons8.com/color/48/null/instagram-new--v1.png"
-        /></a>
-        <a href="#"
-          ><img src="https://img.icons8.com/color/48/null/twitter--v1.png"
-        /></a>
-        <a href="#"
-          ><img src="https://img.icons8.com/color/48/null/youtube-play.png"
-        /></a>
+      </p>
+      <p class="fw-bolder">
+        <a href="#"><img src="https://img.icons8.com/color/48/null/instagram-new--v1.png"/></a>
+        <a href="#"><img src="https://img.icons8.com/color/48/null/twitter--v1.png"/></a>
+        <a href="#"><img src="https://img.icons8.com/color/48/null/youtube-play.png"/></a>
       </p>
       <p class="text-center text-decoration-underline fw-bolder">
-        •South Africa | Cape Town •
+        •South Africa | Cape Town | 8001 | Thomas Street | Jesse House | 22nd Floor Building 223•
       </p>
     </footer>
   </div>
@@ -29,5 +24,8 @@ footer {
   margin-top: 5rem;
   width: 100vw;
   bottom: 0;
+}
+.copy{
+margin: auto;
 }
 </style>
