f82fb476c56b8bf3ef86d2a71b311a17ef2f36af
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-09 10:06:41 +0200

    HomeView

--- stygian-umbra-gaming/src/views/HomeView.vue
+++ stygian-umbra-gaming/src/views/HomeView.vue
@@ -1,19 +1,107 @@
 <template>
-  <div class="home">
+<div class="home">
     <section class="container-fluid sect">
       <div class="wrapper text-center">
           <div class="rounded-3 animated-charcter" data-aos="fade-down">
-              <h2 class="text-white kage display-6 fw-bolder">Portfolio Of</h2>
+              <h1 class="text-white kage display-5 fw-bolder">Stygian Umbra Gaming</h1>
           </div>
-          <figure>
-              
-              <figcaption class="animated-charcter fw-semibold">
-                  <p class="text-light"><strong>Aspiring Developer | Aspiring Web Designer | Aspiring Coder</strong></p>
-              </figcaption>
-          </figure>
+          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7" aria-label="Slide 8"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8" aria-label="Slide 9"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="9" aria-label="Slide 10"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://i.postimg.cc/Hs9KxMSK/Assasins-Creed-Valhalla.jpg" class="d-block w-100" alt="Game1" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Assassins Creed Valhalla</h5>
+                  <p>Some representative placeholder content for the first slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/W431m0Cd/Forza-Horizon-5.jpg" class="d-block w-100" alt="Game2" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Forza Horizon 5</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/bYLPVdbQ/Fortnite.jpg" class="d-block w-100" alt="Game3" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Fortnite</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/xdMn3ggT/dishonored-5.jpg" class="d-block w-100" alt="Game4" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Dishonoured</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/jS9y7qkY/Resident-Evil-Village.png" class="d-block w-100" alt="Game5" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Resident Evil Village</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/x1S2TLnw/Halo-Infinite.png" class="d-block w-100" alt="Game6" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Halo Infinite</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/s2hHBNmK/masseffect3-sq-1641945184920.jpg" class="d-block w-100" alt="Game7" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Mass Effect 3</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/T3LpBzTR/The-Elder-Scrolls-Online-Tamriel-Unlimited.jpg" class="d-block w-100" alt="Game8" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>The Elder Scrolls Online Tamriel Unlimited</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/m2QmG5VG/Logo-of-Stardew-Valley.png" class="d-block w-100" alt="Game9" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Stardew Valley.</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://i.postimg.cc/ZKKx1596/Elden-Ring.jpg" class="d-block w-100" alt="Game10" loading="lazy">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Elden Ring</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
       </div>
   </section>
-  </div>
+</div>
 </template>
 
 <script>
