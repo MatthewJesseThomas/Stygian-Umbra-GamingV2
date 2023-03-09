600244c8143b561f5aaa3462a5646428f8e5e028
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-09 12:10:34 +0200

    HomeView.vue

--- stygian-umbra-gaming/src/views/HomeView.vue
+++ stygian-umbra-gaming/src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
 <div class="home">
+    <h1 class="text-white kage display-3 fw-bolder">Stygian Umbra Gaming</h1>
     <section class="container-fluid sect">
       <div class="wrapper text-center">
-          <div class="rounded-3 animated-charcter" data-aos="fade-down">
-              <h1 class="text-white kage display-5 fw-bolder">Stygian Umbra Gaming</h1>
-          </div>
+          <br>
           <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
             <div class="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -20,70 +19,70 @@
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="https://i.postimg.cc/Hs9KxMSK/Assasins-Creed-Valhalla.jpg" class="d-block w-100" alt="Game1" loading="lazy">
+                <img src="https://i.postimg.cc/Hs9KxMSK/Assasins-Creed-Valhalla.jpg" class="resize d-block w-100" alt="Game1" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Assassins Creed Valhalla</h5>
                   <p>Some representative placeholder content for the first slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/W431m0Cd/Forza-Horizon-5.jpg" class="d-block w-100" alt="Game2" loading="lazy">
+                <img src="https://i.postimg.cc/W431m0Cd/Forza-Horizon-5.jpg" class="resize d-block w-100" alt="Game2" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Forza Horizon 5</h5>
                   <p>Some representative placeholder content for the second slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/bYLPVdbQ/Fortnite.jpg" class="d-block w-100" alt="Game3" loading="lazy">
+                <img src="https://i.postimg.cc/bYLPVdbQ/Fortnite.jpg" class="resize d-block w-100" alt="Game3" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Fortnite</h5>
                   <p>Some representative placeholder content for the third slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/xdMn3ggT/dishonored-5.jpg" class="d-block w-100" alt="Game4" loading="lazy">
+                <img src="https://i.postimg.cc/xdMn3ggT/dishonored-5.jpg" class="resize d-block w-100" alt="Game4" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Dishonoured</h5>
                   <p>Some representative placeholder content for the second slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/jS9y7qkY/Resident-Evil-Village.png" class="d-block w-100" alt="Game5" loading="lazy">
+                <img src="https://i.postimg.cc/jS9y7qkY/Resident-Evil-Village.png" class="resize d-block w-100" alt="Game5" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Resident Evil Village</h5>
                   <p>Some representative placeholder content for the third slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/x1S2TLnw/Halo-Infinite.png" class="d-block w-100" alt="Game6" loading="lazy">
+                <img src="https://i.postimg.cc/x1S2TLnw/Halo-Infinite.png" class="resize d-block w-100" alt="Game6" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Halo Infinite</h5>
                   <p>Some representative placeholder content for the third slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/s2hHBNmK/masseffect3-sq-1641945184920.jpg" class="d-block w-100" alt="Game7" loading="lazy">
+                <img src="https://i.postimg.cc/s2hHBNmK/masseffect3-sq-1641945184920.jpg" class="resize d-block w-100" alt="Game7" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Mass Effect 3</h5>
                   <p>Some representative placeholder content for the third slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/T3LpBzTR/The-Elder-Scrolls-Online-Tamriel-Unlimited.jpg" class="d-block w-100" alt="Game8" loading="lazy">
+                <img src="https://i.postimg.cc/T3LpBzTR/The-Elder-Scrolls-Online-Tamriel-Unlimited.jpg" class="resize d-block w-100" alt="Game8" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>The Elder Scrolls Online Tamriel Unlimited</h5>
                   <p>Some representative placeholder content for the third slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/m2QmG5VG/Logo-of-Stardew-Valley.png" class="d-block w-100" alt="Game9" loading="lazy">
+                <img src="https://i.postimg.cc/m2QmG5VG/Logo-of-Stardew-Valley.png" class="resize d-block w-100" alt="Game9" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Stardew Valley.</h5>
                   <p>Some representative placeholder content for the third slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://i.postimg.cc/ZKKx1596/Elden-Ring.jpg" class="d-block w-100" alt="Game10" loading="lazy">
+                <img src="https://i.postimg.cc/ZKKx1596/Elden-Ring.jpg" class="resize d-block w-100" alt="Game10" loading="lazy">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Elden Ring</h5>
                   <p>Some representative placeholder content for the third slide.</p>
