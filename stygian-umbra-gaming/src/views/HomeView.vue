c740bd5bf700e9e50283a8d1467fc4d26bbea709
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-14 14:03:31 +0200

    Carousel Slideshow Configured

--- stygian-umbra-gaming/src/views/HomeView.vue
+++ stygian-umbra-gaming/src/views/HomeView.vue
@@ -15,56 +15,56 @@
     <section class="container-fluid sect">
       <div class="wrapper text-center">
           <br>
-          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+          <div id="carouselExampleCaptionsFadeInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7" aria-label="Slide 8"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8" aria-label="Slide 9"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="9" aria-label="Slide 10"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="3" aria-label="Slide 4"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="4" aria-label="Slide 5"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="5" aria-label="Slide 6"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="6" aria-label="Slide 7"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="7" aria-label="Slide 8"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="8" aria-label="Slide 9"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptionsFade" data-bs-slide-to="9" aria-label="Slide 10"></button>
             </div>
             <div class="carousel-inner">
-              <div class="carousel-item active">
+              <div class="carousel-item active" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/QMc0Bpd7/Assassins-Creed-Valhalla.jpg" class="resize d-block w-100" alt="Game1" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/90H4D7zD/Forza-Horizon-5.jpg" class="resize d-block w-100" alt="Game2" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/wMQxzZbT/Fortnite.jpg" class="resize d-block w-100" alt="Game3" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/7Z4ZS4xW/Dishonored-The-Return-of-Daud.jpg" class="resize d-block w-100" alt="Game4" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/XvmMcKfY/Resident8-Evil-Village.jpg" class="resize d-block w-100" alt="Game5" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/QtG4dfg1/Halo-Infinite.jpg" class="resize d-block w-100" alt="Game6" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/5yfswjRt/Mass-Effect-3.jpg" class="resize d-block w-100" alt="Game7" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/zfBxJptX/Elder-Scrolls-Online.jpg" class="resize d-block w-100" alt="Game8" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/pXs82vgz/Stardew-Valley.jpg" class="resize d-block w-100" alt="Game9" loading="lazy">
               </div>
-              <div class="carousel-item">
+              <div class="carousel-item" data-bs-interval="3000">
                 <img src="https://i.postimg.cc/x1WXC1gJ/Elden-Ring.jpg" class="resize d-block w-100" alt="Game10" loading="lazy">
               </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptionsFadeInterval" data-bs-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptionsFadeInterval" data-bs-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Next</span>
             </button>
