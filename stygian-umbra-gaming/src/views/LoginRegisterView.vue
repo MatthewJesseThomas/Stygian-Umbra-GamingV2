23fe505233c6867e1130a3651b1419d26c17e577
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-14 14:48:39 +0200

    LoginRegisterView Configured

--- stygian-umbra-gaming/src/views/LoginRegisterView.vue
+++ stygian-umbra-gaming/src/views/LoginRegisterView.vue
@@ -1,13 +1,28 @@
-<template lang="">
-    <div>
-        
+<template>
+    <div class="test">
+        <h1 class="animated-charcter text-light">•Login•</h1>
+        <br>
+        <div class="login border-dark">
+            <LoginComponent/>
+        </div>
+        <hr class="bg-dark border-2">
+        <h1 class="animated-charcter text-light">•Register•</h1>
+        <br>
+        <div class="register border-dark">
+        <RegisterComponent/>
+        </div>
     </div>
 </template>
 <script>
+import LoginComponent from '../components/LoginComponent.vue'
+import RegisterComponent from '../components/RegisterComponent.vue'
 export default {
-    
-}
+  components: {
+    LoginComponent,
+    RegisterComponent
+  }
+};
 </script>
-<style lang="">
+<style>
     
 </style>
