ce014162f791f28a3f694201c1ec1fdfa8ec7118
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-14 15:03:59 +0200

    RegisterComponent.vue

--- stygian-umbra-gaming/src/components/RegisterComponent.vue
+++ stygian-umbra-gaming/src/components/RegisterComponent.vue
@@ -1,13 +1,46 @@
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <form class="text-white">
+        <div class="form-group">
+          <label for="exampleInputFirstName">FirstName</label>
+          <input type="text" class="form-control" id="exampleInputFirstName" placeholder="Enter FirstName">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputLastName">Password</label>
+          <input type="text" class="form-control" id="exampleInputLastName" placeholder="Enter LastName">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputGender">Gender</label>
+          <input type="text" class="form-control" id="exampleInputGender"  placeholder="Enter Gender">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputCellphoneNumber">CellPhoneNumber</label>
+          <input type="number" class="form-control" id="exampleInputCellphoneNumber" placeholder="Enter CellPhoneNumber">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmailAdd">Email Address</label>
+          <input type="email" class="form-control" id="exampleInputEmailAdd" aria-describedby="emailHelp" placeholder="Enter Email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputUserRole">User Role</label>
+          <input type="text" class="form-control" id="exampleInputUserRole" placeholder="Enter UserRole">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlFile1">User Profile</label>
+            <br>
+            <input type="file" class="form-control-file" id="exampleFormControlFile1 userProfile">
+          </div>
+        <button type="submit" class="btn btn-primary">Register</button>
+      </form>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
+<style>
     
 </style>
