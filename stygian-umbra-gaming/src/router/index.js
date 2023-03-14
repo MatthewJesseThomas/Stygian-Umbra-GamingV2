26d5ab5346f97889b5e0ecfba935686168343b78
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-14 09:48:00 +0200

    Router Updated

--- stygian-umbra-gaming/src/router/index.js
+++ stygian-umbra-gaming/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ContactView from '../views/ContactView.vue'
+import ProductView from '../views/ProductView.vue'
+import CartView from '../views/CartView.vue'
+import LoginRegisterView from '../views/LoginRegisterView.vue'
+import UserView from '../views/UserView.vue'
+import AdminView from '../views/AdminView.vue'
 
 const routes = [
   {
@@ -23,6 +28,46 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: ContactView
+  },
+  {
+    path: '/product',
+    name: 'product',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: CartView
+  },
+  {
+    path: '/loginregister',
+    name: 'loginregister',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: LoginRegisterView
+  },
+  {
+    path: '/user',
+    name: 'user',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: UserView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: AdminView
   }
 ]
 
