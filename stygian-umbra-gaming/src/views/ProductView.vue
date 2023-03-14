f45f9755e86d2f51fe40b1092579fce599e68b4c
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-14 14:20:27 +0200

    ProductView Configured

--- stygian-umbra-gaming/src/views/ProductView.vue
+++ stygian-umbra-gaming/src/views/ProductView.vue
@@ -1,13 +1,21 @@
-<template lang="">
-    <div>
-        
+<template>
+    <div class="test">
+        <h1 class="animated-charcter text-light">•Our Products•</h1>
+        <br>
+        <h2 class="animated-charcter text-light">•As Follows•</h2>
+        <ProductComponent/>
     </div>
 </template>
 <script>
+import ProductComponent from '../components/ProductComponent.vue'
 export default {
-    
-}
+  components: {
+    ProductComponent
+  }
+};
 </script>
-<style lang="">
-    
+<style>
+.test{
+    margin-top: 9rem;
+  }  
 </style>
