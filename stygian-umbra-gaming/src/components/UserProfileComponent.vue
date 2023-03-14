a94751553c770bf597b544c73d2704b3e30fdb63
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-14 16:12:55 +0200

    UserProfileComponent Created - Configured

--- stygian-umbra-gaming/src/components/UserProfileComponent.vue
+++ stygian-umbra-gaming/src/components/UserProfileComponent.vue
@@ -1,7 +1,145 @@
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <section style="background-color: #eee;">
+        <div class="container py-5">
+          <div class="row">
+            <div class="col">
+              <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
+                <ol class="breadcrumb mb-0">
+                  <li class="breadcrumb-item"><a href="http://localhost:8080/">Home</a></li>
+                  <li class="breadcrumb-item"><a href="http://localhost:8080/user">User</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">User Profile</li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+      
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="card mb-4">
+                <div class="card-body text-center">
+                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+                    class="rounded-circle img-fluid" style="width: 150px;">
+                  <h5 class="my-3">John Smith</h5>
+                  <p class="text-muted mb-1">Stygian Umbra Ūsus(User)</p>
+                  <p class="text-muted mb-4">Strandfontein, Mitchell's Palin, Cape Town</p>
+                  <div class="d-flex justify-content-center mb-2">
+                    <button type="button" class="btn btn-warning">Edit</button>
+                    <button type="button" class="btn btn-outline-danger ms-1">Delete</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-8">
+              <div class="card mb-4">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <p class="mb-0">Full Name</p>
+                    </div>
+                    <div class="col-sm-9">
+                      <p class="text-muted mb-0">Johnatan Smith</p>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <p class="mb-0">Email</p>
+                    </div>
+                    <div class="col-sm-9">
+                      <p class="text-muted mb-0">example@example.com</p>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <p class="mb-0">Phone</p>
+                    </div>
+                    <div class="col-sm-9">
+                      <p class="text-muted mb-0">(097) 234-5678</p>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <p class="mb-0">Mobile</p>
+                    </div>
+                    <div class="col-sm-9">
+                      <p class="text-muted mb-0">(098) 765-4321</p>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <p class="mb-0">Address</p>
+                    </div>
+                    <div class="col-sm-9">
+                      <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="card mb-4 mb-md-0">
+                    <div class="card-body">
+                      <p class="mb-4"><span class="text-primary font-italic me-1 Display-4">Cart1</span>
+                      </p>
+                      <p class="mb-1 display-3" style="font-size: .77rem;">Order ID</p>
+                      <div class="" style="height: 5px;">
+                        <p>1</p>
+                      </div>
+                      <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">Order Name</p>
+                      <div class="" style="height: 5px;">
+                        <p>#001</p>
+                      </div>
+                      <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">Product ID</p>
+                      <div class="" style="height: 5px;">
+                        <p>1</p>
+                      </div>
+                      <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">QTY</p>
+                      <div class="" style="height: 5px;">
+                        <p>10</p>
+                      </div>
+                      <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">User ID</p>
+                      <div class="" style="height: 5px;">
+                        <p>1</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4 mb-md-0">
+                      <div class="card-body">
+                        <p class="mb-4"><span class="text-primary font-italic me-1 Display-4">Cart2</span>
+                        </p>
+                        <p class="mb-1 display-3" style="font-size: .77rem;">Order ID</p>
+                        <div class="" style="height: 5px;">
+                          <p>1</p>
+                        </div>
+                        <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">Order Name</p>
+                        <div class="" style="height: 5px;">
+                          <p>#002</p>
+                        </div>
+                        <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">Product ID</p>
+                        <div class="" style="height: 5px;">
+                          <p>5</p>
+                        </div>
+                        <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">QTY</p>
+                        <div class="" style="height: 5px;">
+                          <p>3</p>
+                        </div>
+                        <p class="mt-4 mb-1 Display-3" style="font-size: .77rem;">User ID</p>
+                        <div class="" style="height: 5px;">
+                          <p>1</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 </template>
 <script>
 export default {
