36c0642103fca51674cec25a711f7500f9953e99
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-03-14 16:52:58 +0200

    AdminView Setup - Configured

--- stygian-umbra-gaming/src/views/AdminView.vue
+++ stygian-umbra-gaming/src/views/AdminView.vue
@@ -1,12 +1,26 @@
 <template lang="">
-    <div>
-        
+    <div class="test">
+        <h1 class="animated-charcter text-light">•Administrator•</h1>
+        <br>
+        <h2 class="animated-charcter text-light">•Admin Crud's•</h2>
+        <UsersCrudComponent/>
+        <br>
+        <ProductCrudComponent/>
+        <br>
+        <CartComponent/>
     </div>
 </template>
 <script>
+import UsersCrudComponent from '../components/UsersCrudComponent.vue'
+import ProductCrudComponent from '../components/ProductCrudComponent.vue'
+import CartComponent from '../components/CartComponent.vue'
 export default {
-    
-}
+  components: {
+    UsersCrudComponent,
+    ProductCrudComponent,
+    CartComponent
+  }
+};
 </script>
 <style lang="">
     
